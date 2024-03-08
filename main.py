c96ef423189fa70512fa32a10939c0b55a120bfe
Tomas Slusny <slusnucky@gmail.com>
2024-03-08 19:10:35 +0100

    Optimize the models
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import chromadb
+import subprocess
+import os
+import time
+from chromadb.utils import embedding_functions
 from fastapi import FastAPI
 from pydantic import BaseModel
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+from watchdog.events import FileSystemEvent, FileSystemEventHandler
 
 class Watch(BaseModel):
     dir: str
@@ -19,30 +23,68 @@ class FileChangeHandler(FileSystemEventHandler):
         if event.is_directory:
             return
 
-        file_contents = open(event.src_path).read()
+        if "/.git" in event.src_path:
+            return
+
+        try:
+            file_contents = open(event.src_path).read()
+        except:
+            return
+
+        print(event.src_path)
         self.collection.add(
             documents = [ file_contents ],
             ids = [ event.src_path ],
         )
 
 app = FastAPI()
-db = chromadb.PersistentClient(path="~/.cache/chromadb")
+db = chromadb.PersistentClient(path=os.getenv("HOME") + "/.cache/chromadb")
+embedding_function = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="multi-qa-MiniLM-L6-cos-v1")
 observer = Observer()
 observer.start()
 watches = {}
 
+def git_files(dir):
+    files = subprocess.run(
+        ["git", "-C", dir, "ls-files", "--full-name"],
+        capture_output=True
+    ).stdout.decode().split("\n")
+    files = [os.path.join(dir, file) for file in files]
+    for file in files:
+        print(file)
+    return [file for file in files if os.path.isfile(file)]
+
 def col(dir):
-    return db.get_or_create_collection(dir.replace("/", ""))
+    return db.get_or_create_collection(
+        name=dir.replace("/", ""),
+        metadata={"hnsw:space": "cosine"},
+        embedding_function=embedding_function
+    )
 
 @app.post("/start")
 def start(watch: Watch):
-    watches[watch.dir] = observer.schedule(FileChangeHandler(col(watch.dir)), watch.dir, recursive=True)
-    return watch
+    start_time = time.time()
+    handler = FileChangeHandler(col(watch.dir))
+    files = git_files(watch.dir)
+    for file in files:
+        handler.on_modified(FileSystemEvent(src_path=file))
+    end = time.time()
+    duration = end - start_time
+    watches[watch.dir] = observer.schedule(handler, watch.dir, recursive=True)
+    return {
+        'dir': watch.dir,
+        'files': len(files),
+        'duration': duration,
+    }
 
 @app.post("/stop")
 def stop(watch: Watch):
     observer.unschedule(watches[watch.dir])
     return watch
+
+@app.post("/count")
+def count(watch: Watch):
+    return col(watch.dir).count()
 
 @app.post("/query")
 def query(query: Query):
